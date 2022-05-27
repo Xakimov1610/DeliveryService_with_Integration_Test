88c587d40f3baa7cace2d705ac3051b81a99716a
Xakimov1610 <nosirbekxakimov01@gmail.com>
2022-05-28 03:26:02 +0500

    Add project files.

---
+++ DeliveryServiceApi/Controllers/DeliveryController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DeliveryServiceApi.Controllers
+{
+    [Route(template:"api/[controller]")]
+    [Controller]
+    public class DeliveryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
